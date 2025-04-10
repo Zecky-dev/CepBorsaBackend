692fd12d3a6338a05d507ad63bf0343f4f7fbe26
Zecky-dev <l2021012041@ogr.sdu.edu.tr>
2025-04-10 21:26:51 +0300

    Listen ip address changed.

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
-import express from 'express'
-import swaggerOptions from './config/swaggerOptions.js'
-import swaggerUi from 'swagger-ui-express'
-import swaggerJSDoc from 'swagger-jsdoc'
+import express from "express";
+import swaggerOptions from "./config/swaggerOptions.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
-import 'dotenv/config'
+import "dotenv/config";
 
-const swaggerDocs = swaggerJSDoc(swaggerOptions)
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Routes
-import stockRoutes from './routes/stock.js'
-import botRoutes from './routes/bot.js'
-import currencyRoutes from './routes/currency.js'
-import userRoutes from './routes/user.js'
+import stockRoutes from "./routes/stock.js";
+import botRoutes from "./routes/bot.js";
+import currencyRoutes from "./routes/currency.js";
+import userRoutes from "./routes/user.js";
 
 // Body parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true, limit: "100mb" }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
-app.get('/', (req, res) => {
-    res.send('Hoş geldiniz! API dökümantasyonu için /api/docs yolunu ziyaret edebilirsiniz.')
-})
+app.get("/", (req, res) => {
+  res.send(
+    "Hoş geldiniz! API dökümantasyonu için /api/docs yolunu ziyaret edebilirsiniz."
+  );
+});
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-app.use('/api/stock', stockRoutes)
-app.use('/api/bot', botRoutes)
-app.use('/api/currency', currencyRoutes)
-app.use('/api/user', userRoutes);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api/stock", stockRoutes);
+app.use("/api/bot", botRoutes);
+app.use("/api/currency", currencyRoutes);
+app.use("/api/user", userRoutes);
 
 // 404 for other endpoints
 app.use((_, res) => {
-    res.status(404).json({
-        statusCode: 404,
-        error: "Ulaşmaya çalıştığınız nokta bulunmuyor!"
-    })
-})
+  res.status(404).json({
+    statusCode: 404,
+    error: "Ulaşmaya çalıştığınız nokta bulunmuyor!",
+  });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running at port ${PORT}`);
+});
